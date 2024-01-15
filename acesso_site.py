0c16c5073679d030d11fb14a4852bec7a82c4422
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-15 16:10:22 -0300

    acessando o site

--- acesso_site.py
+++ acesso_site.py
@@ -1,4 +1,24 @@
 from inicializar import GoogleChrome
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+# Site os dados serão extráidos
+SITE = 'https://telefonesimportados.netlify.app/'
 
 browser = GoogleChrome()
 driver, wait = browser.get_driver(), browser.get_wait()
+
+
+driver.get(SITE)
+sleep(5)
+
+driver.execute_script("window.scrollBy(0, 2500);")
+
+
+
+
+
+
+
+
