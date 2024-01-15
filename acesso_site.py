b7ec5bb6ec063afb0731c3ed0df78baf142d29e0
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-15 18:12:52 -0300

    extração de dados do site

--- acesso_site.py
+++ acesso_site.py
@@ -1,6 +1,7 @@
 from inicializar import GoogleChrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 
 
@@ -11,25 +12,38 @@ SITE = 'https://telefonesimportados.netlify.app/'
 MARCAS = '//div[@class="single-shop-product"]//h2/a'
 PRECOS = '//div[@class="single-shop-product"]//div[@class="product-carousel-price"]/ins'
 
+# Botão que acessa a próxima página do site.
+PROXIMA_PAGINA = '//a[@aria-label="Next"]'
+
 browser = GoogleChrome()
 driver, wait = browser.get_driver(), browser.get_wait()
 
 
 driver.get(SITE)
-sleep(5)
 
-driver.execute_script("window.scrollBy(0, 2500);")
+while True:
+    sleep(3)
 
-marcas = wait.until(expected_conditions.visibility_of_all_elements_located(
-    (By.XPATH, MARCAS)))
+    driver.execute_script("window.scrollBy(0, 2500);")
 
-precos = wait.until(expected_conditions.visibility_of_all_elements_located(
-    (By.XPATH, PRECOS)))
+    marcas = wait.until(expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, MARCAS)))
 
+    precos = wait.until(expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, PRECOS)))
 
-for marca, preco in zip(marcas, precos):
-    print(marca.text + " " + preco.text)
+    # A lógica de adicionar os dados à planilha entra aqui:
+    for marca, preco in zip(marcas, precos):
+        print(marca.text + " " + preco.text)
 
+    try:
+        proxima_pagina = wait.until(expected_conditions.element_to_be_clickable(
+            (By.XPATH, PROXIMA_PAGINA)))
+        proxima_pagina.click()
+    except TimeoutException:
+        break
+print("Acabamos")
+    
 
 
 
