b8b58a0ac29c00850760292372afce82b76dfe25
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-15 16:38:33 -0300

    obtendo marca e preco de celulares em uma página

--- acesso_site.py
+++ acesso_site.py
@@ -1,10 +1,15 @@
 from inicializar import GoogleChrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from time import sleep
 
 
 # Site os dados serão extráidos
 SITE = 'https://telefonesimportados.netlify.app/'
+
+# MARCAS E PRECOS contém o DOM da marca e preço de cada celular presente no site.
+MARCAS = '//div[@class="single-shop-product"]//h2/a'
+PRECOS = '//div[@class="single-shop-product"]//div[@class="product-carousel-price"]/ins'
 
 browser = GoogleChrome()
 driver, wait = browser.get_driver(), browser.get_wait()
@@ -15,7 +20,15 @@ sleep(5)
 
 driver.execute_script("window.scrollBy(0, 2500);")
 
+marcas = wait.until(expected_conditions.visibility_of_all_elements_located(
+    (By.XPATH, MARCAS)))
 
+precos = wait.until(expected_conditions.visibility_of_all_elements_located(
+    (By.XPATH, PRECOS)))
+
+
+for marca, preco in zip(marcas, precos):
+    print(marca.text + " " + preco.text)
 
 
 
